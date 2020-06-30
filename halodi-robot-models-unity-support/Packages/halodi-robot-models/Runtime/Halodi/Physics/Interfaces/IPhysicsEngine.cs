f730e6f9efc4ebf59ad316e2ab08bdbe1d19a7f9
Jesper Smith <jesper@halodi.com>
2020-06-30 10:36:39 +0200

    Add simulate call to robot

--- halodi-robot-models-unity-support/Packages/halodi-robot-models/Runtime/Halodi/Physics/Interfaces/IPhysicsEngine.cs
+++ halodi-robot-models-unity-support/Packages/halodi-robot-models/Runtime/Halodi/Physics/Interfaces/IPhysicsEngine.cs
@@ -12,9 +12,6 @@ namespace Halodi.Physics.Interfaces
         public abstract IFixedJointPhysics AddFixedJoint(ArticulationFixedJoint joint);
 
         public abstract IFloatingJointPhysics AddFloatingJoint(ArticulationFloatingJoint joint);
-
-
-        public abstract void Simulate(float step);
     }
 
 }
