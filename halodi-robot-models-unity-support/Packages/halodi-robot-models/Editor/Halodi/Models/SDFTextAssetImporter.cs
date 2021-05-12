7b7a662a39bfc217e6160d06b511c6f3d7ed2098
Jesper Smith <jesper@halodi.com>
2021-05-12 15:18:43 +0200

    Added asset importer for SDF files

---
+++ halodi-robot-models-unity-support/Packages/halodi-robot-models/Editor/Halodi/Models/SDFTextAssetImporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+namespace Halodi.Models
+{
+    [ScriptedImporter(1, "sdf")]
+    public class SDFTextAssetImporter : ScriptedImporter
+    {
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            TextAsset subAsset = new TextAsset(File.ReadAllText(ctx.assetPath));
+            ctx.AddObjectToAsset("text", subAsset);
+            ctx.SetMainObject(subAsset);
+        }
+
+    }
+}
